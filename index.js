d6315bbf0caae6ff11c38e0b3103037efc4bdbd8
Ravi Teja <rpillala@iu.edu>
2024-01-29 23:15:38 +0000

    Helper functions are utilized, and distance conversion can be performed based on user inputs

--- index.js
+++ index.js
@@ -23,23 +23,23 @@ exports.MF = (req,res) => {
     let feetValue = req.query.feetValue;
     console.log(`input feetValue`);
     // console.log(`${feetValue} meters is ${Distance.calculate('meters', feetValue)} feet.`);
-    res.send(`${feetValue} meters is ${Distance.calculate('meters', feetValue)} feet.`);
+    res.status(200).send(`${feetValue} meters is ${Distance.calculate('meters', feetValue)} feet.`);
 }
 exports.FM = (req,res) => {
     let metersValue = req.query.metersValue;
     console.log(`input metersValue`);
     // console.log(`${metersValue} feet is ${Distance.calculate('feet', metersValue)} meters.`);
-    res.send(`${metersValue} feet is ${Distance.calculate('feet', metersValue)} meters.`);
+    res.status(200).send(`${metersValue} feet is ${Distance.calculate('feet', metersValue)} meters.`);
 }
 exports.MK = (req,res) => {
     let milesValue = req.query.milesValue;
     console.log(`input milesValue`);
     // console.log(`${milesValue} miles is ${Distance.calculate('miles', milesValue)} kilometers.`);
-    res.send(`${milesValue} miles is ${Distance.calculate('miles', milesValue)} kilometers.`)
+    res.status(200).send(`${milesValue} miles is ${Distance.calculate('miles', milesValue)} kilometers.`);
 }
 exports.KM = (req,res) => {
     let kilometersValue = req.query.kilometersValue;
     console.log(`input kilometersValue`);
     // console.log(`${kilometersValue} kilometers is ${Distance.calculate('kilometers', kilometersValue)} miles.`);
-    res.send(`${kilometersValue} kilometers is ${Distance.calculate('kilometers', kilometersValue)} miles.`);
+    res.status(200).send(`${kilometersValue} kilometers is ${Distance.calculate('kilometers', kilometersValue)} miles.`);
 }
